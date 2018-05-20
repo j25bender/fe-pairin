c015d86f7c3a07806da5920e8c5b97b21f19c1c7
Jeff Bender <j25bender@gmail.com>
2018-05-19 22:09:18 -0600

    Added UserList Component

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import UserList from '../UserList/UserList'
 import './SignIn.css';
 
 export class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    }
+  }
+
+  validateInput = async () => {
+    const { email, password } = this.state;
+    if(email.length && password.length) {
+      try {
+        const initialFetch = await fetch('/authenticate', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            "username": email,
+            "password": password
+          })
+        })
+        const authResponse = await initialFetch.json();
+      } catch(error) {
+        this.setState({ error })
+      }
+    }
+    document.getElementById('email-input').value = '';
+    document.getElementById('password-input').value = '';    
+  }
+
   render() {
     return (
       <div>
         <header>
-          <img src={require('../../assets/pairin-logo.png')} 
+          <img src={ require('../../assets/pairin-logo.png') } 
                alt='PAIRIN Company Logo' />
                 <span>
                   PAIRIN
@@ -14,21 +46,34 @@ export class SignIn extends Component {
         </header>
         <div id='form-container'>
           <form>
-            <h5 id='email-label'>Email Address</h5>
-            <input type='email' 
-                  id='email-input' 
-                  placeholder='email'
-                  autoFocus 
-                  required />
-            <h5 id='password-label'>Password</h5>
+            <h5 id='email-label'>
+              Email Address
+            </h5>
+            <input type='email'
+                   value={ this.state.email }
+                   className='input'
+                   id='email-input'
+                   placeholder='email'
+                   autoComplete='email'
+                   autoFocus 
+                   required 
+                   onChange={ e => this.setState({ email: e.target.value })} 
+            />
+            <h5 id='password-label'>
+              Password
+            </h5>
             <input type='password'
-                  id='password-input' 
-                  placeholder='password' 
-                  required />
-            <input type='button'
+                   value={ this.state.password }
+                   id='password-input'
+                   placeholder='password'
+                   autoComplete='current-password'
+                   required
+                   onChange={ e => this.setState({ password: e.target.value })} 
+            />
+            <input type='submit'
                    value='SIGN IN'
-                   id='sign-in-button' 
-                   disabled
+                   id='sign-in-button'
+                   onClick={ () => this.validateInput() }
             />
           </form>
         </div>
---
+++ src/Components/UserList/UserList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import SignIn from '../SignIn/SignIn'
+import PropTypes from 'prop-types'
+
+class UserList extends Component {
+  constructor(props) {
+    super(props)
+    console.log(props)
+  }
+
+  // componentWillMount() {
+
+  // }
+
+  componentDidMount() {
+
+  }
+
+  // componentWillReceiveProps(nextProps) {
+
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+
+  // }
+
+  // componentWillUpdate(nextProps, nextState) {
+
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+
+  // }
+
+  // componentWillUnmount() {
+
+  // }
+
+  render() {
+    return (
+      <div>
+        HI
+      </div>
+    )
+  }
+}
+
+UserList.propTypes = {
+
+}
+
+export default UserList
